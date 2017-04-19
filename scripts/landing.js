4bfe3dc39b5a22ae2ff8d5c9babcf97cd2a7ec7a
sean21502 <seanhite@gmail.com>
2017-04-18 21:51:17 -0400

    spacing adjustment

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,14 +1,16 @@
 var animatePoints = function() {
  
                 var points = document.getElementsByClassName('point');
-                var revealPoint = function(x) {
-                     points[x].style.opacity = 1;
-                     points[x].style.transform = "scaleX(1) translateY(0)";
-                     points[x].style.msTransform = "scaleX(1) translateY(0)";
-                     points[x].style.WebkitTransform = "scaleX(1) translateY(0)";
+                
+				var revealPoint = function (index) {
+                     
+					 points[index].style.opacity = 1;
+                     points[index].style.transform = "scaleX(1) translateY(0)";
+                     points[index].style.msTransform = "scaleX(1) translateY(0)";
+                     points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
                 }
     
-                for(var i = 0; i <points.length; i++) {
+                for(var i = 0; i < points.length; i++) {
                     revealPoint(i)
                 }
              };
