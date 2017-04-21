ebcae01b87884ed30d607bd571bd2dff338e61b2
sean21502 <seanhite@gmail.com>
2017-04-20 21:18:28 -0400

    checkpoint 9

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,8 +1,8 @@
-var animatePoints = function () {
+var pointsArray = document.getElementsByClassName('point');
  
-                var points = document.getElementsByClassName('point');
+var animatePoints = function(points) {
                 
-				var revealPoint = function (index) {
+				var revealPoint = function(index) {
                      
 					 points[index].style.opacity = 1;
                      points[index].style.transform = "scaleX(1) translateY(0)";
@@ -14,4 +14,18 @@ var animatePoints = function () {
                     revealPoint(i)
                 }
              };
-animatePoints ();
+window.onload = function () {
+	if (window.innerHeight > 950) {
+         animatePoints(pointsArray);
+     }
+	
+	var sellingPoints = document.getElementsByClassName('clearfix')[0];
+	var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+
+	
+	window.addEventListener('scroll', function(event) {
+		if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+             animatePoints(pointsArray);   
+         }
+	});
+}
