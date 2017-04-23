ede7a37c5cfc57caedb2b137825e88df16afe32b
sean21502 <seanhite@gmail.com>
2017-04-23 14:10:30 -0400

    Assignment 11

--- scripts/album.js
+++ scripts/album.js
@@ -26,35 +26,46 @@
          { title: 'Wrong phone number', duration: '2:15'}
      ]
  };
+ var albumImagineDragons = {
+     title: 'Night Visions',
+     artist: 'Imagine Dragons',
+     label: 'Interscope',
+     year: '2012',
+     albumArtUrl: 'https://upload.wikimedia.org/wikipedia/en/3/3f/Night_Visions_Album_Cover.jpeg',
+     songs: [
+         { title: 'Radioactive', duration: '3:06' },
+         { title: 'Tiptoe', duration: '3:14' },
+         { title: 'It;s Time', duration: '4:00'},
+         { title: 'Demons', duration: '2:57' },
+         { title: 'On Top of the World', duration: '3:12'}
+     ]
+ };
 var createSongRow = function(songNumber, songName, songLength) {
      var template =
         '<tr class="album-view-song-item">'
       + '  <td class="song-item-number">' + songNumber + '</td>'
       + '  <td class="song-item-title">' + songName + '</td>'
       + '  <td class="song-item-duration">' + songLength + '</td>'
-      + '</tr>'
-      ;
+      + '</tr>';
  
      return template;
  };
+
+var albumTitle = document.getElementsByClassName('album-view-title')[0];
+var albumArtist = document.getElementsByClassName('album-view-artist')[0];
+var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
+var albumImage = document.getElementsByClassName('album-cover-art')[0];
+var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
+
 var setCurrentAlbum = function(album) {
-     // #1
-     var albumTitle = document.getElementsByClassName('album-view-title')[0];
-     var albumArtist = document.getElementsByClassName('album-view-artist')[0];
-     var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
-     var albumImage = document.getElementsByClassName('album-cover-art')[0];
-     var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
- 
-     // #2
-     albumTitle.firstChild.nodeValue = album.title;
+     
+	 albumTitle.firstChild.nodeValue = album.title;
      albumArtist.firstChild.nodeValue = album.artist;
      albumReleaseInfo.firstChild.nodeValue = album.year + ' ' + album.label;
      albumImage.setAttribute('src', album.albumArtUrl);
- 
-     // #3
+
      albumSongList.innerHTML = '';
- 
-     // #4
+
      for (var i = 0; i < album.songs.length; i++) {
          albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
      }
@@ -62,4 +73,14 @@ var setCurrentAlbum = function(album) {
  
  window.onload = function() {
      setCurrentAlbum(albumPicasso);
+	 
+	 var albums = [albumPicasso, albumMarconi, albumImagineDragons];
+	 var i = 1;
+	 albumImage.addEventListener("click", function(event) {
+		setCurrentAlbum(albums[i]);
+		i++;
+		if (i == albums.length) {
+			i == 0;
+		}					 						 
+	});
  };
