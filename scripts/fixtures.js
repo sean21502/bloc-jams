f6c6aa142f7be4fde263e0d69a431c9b5a6326f6
sean21502 <seanhite@gmail.com>
2017-05-04 22:00:39 -0400

    checkpoint 19

---
+++ scripts/fixtures.js
@@ -0,0 +1,44 @@
+var albumPicasso = {
+     title: 'The Colors',
+     artist: 'Pablo Picasso',
+     label: 'Cubism',
+     year: '1881',
+     albumArtUrl: 'assets/images/album_covers/01.png',
+     songs: [
+         { title: 'Blue', duration: '4:26' },
+         { title: 'Green', duration: '3:14' },
+         { title: 'Red', duration: '5:01' },
+         { title: 'Pink', duration: '3:21' },
+         { title: 'Magenta', duration: '2:15' }
+     ]
+ };
+ 
+ var albumMarconi = {
+     title: 'The Telephone',
+     artist: 'Guglielmo Marconi',
+     label: 'EM',
+     year: '1909',
+     albumArtUrl: 'assets/images/album_covers/20.png',
+     songs: [
+         { title: 'Hello, Operator?', duration: '1:01' },
+         { title: 'Ring, ring, ring', duration: '5:01' },
+         { title: 'Fits in your pocket', duration: '3:21' },
+         { title: 'Can you hear me now?', duration: '3:14' },
+         { title: 'Wrong phone number', duration: '2:15' }
+     ]
+ };
+
+var albumImagineDragons = {
+     title: 'Night Visions',
+     artist: 'Imagine Dragons',
+     label: 'Interscope',
+     year: '2012',
+     albumArtUrl: 'https://upload.wikimedia.org/wikipedia/en/3/3f/Night_Visions_Album_Cover.jpeg',
+     songs: [
+         { title: 'Radioactive', duration: '3:06' },
+         { title: 'Tiptoe', duration: '3:14' },
+         { title: 'Its Time', duration: '4:00' },
+         { title: 'Demons', duration: '2:57' },
+         { title: 'On Top of the World', duration: '3:12' }
+     ]
+ };
